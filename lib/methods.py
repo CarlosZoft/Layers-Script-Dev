b26669d509462b1424c8dd80423409e8eec284a0
CarlosZoft <carlosoftwar@gmail.com>
2021-12-10 20:04:09 -0300

    feat: adding tilte in terminal that run services

--- lib/methods.py
+++ lib/methods.py
@@ -5,5 +5,5 @@ def wait(seconds):
     time.sleep(seconds)
 
 def run_service(path_script, directory, flags):
-    command = f"gnome-terminal --tab -- bash {path_script} -p {directory} {flags}"
+    command = f"gnome-terminal --tab --title={directory} -- bash {path_script} -p {directory} {flags}"
     os.system(command)
