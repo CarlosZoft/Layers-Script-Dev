ed00a568925af6763eb3c9a2844346353fff7a1a
CarlosZoft <carlosoftwar@gmail.com>
2022-01-12 22:06:08 -0300

    fix: add_apps doesnt working

--- main.py
+++ main.py
@@ -6,21 +6,6 @@ import os
 get_path_repo.dist_find()
 path_script = os.path.dirname(os.path.abspath(__file__)) + '/script.sh'
 
-addApps = input("Do you want to add apps to running? (y/n): ")
-
-# List of apps available
-for idUnique, app in enumerate(repositorys.apps.keys()):
-    print(f"\n{idUnique} - {app}")
-
-# Select apps to run
-selectedApps = input(f"\nselect the apps with format : '0,1,2':\n");
-
-# Running default repositorys
-for i in repositorys.data:
-    print(f"prepare to run {i['type']} on {i['directory']}")
-    methods.wait(5)
-    methods.run_service(path_script, i['directory'], i['flag'])
-
 # Running apps
 def run_app(currentRepo, relativePath ):
     path = currentRepo[relativePath]
@@ -28,20 +13,38 @@ def run_app(currentRepo, relativePath ):
     methods.run_service(path_script, path["directory"], path["flag"])
     methods.wait(5)
 
-# Worker selected apps
-for i in selectedApps.split(','):  
-    indexOfApp = int(i)
-    listOfApps = list(repositorys.apps.keys())
-    if(indexOfApp < len(listOfApps)):
-        key = listOfApps[int(indexOfApp)]
-        # if have docker, run it
-        if(key in repositorys.docker.keys()):
-            run_app(repositorys.docker[key], 'docker')
-        #  if have interface, run it
-        if("interface" in repositorys.apps[key]):
-            run_app(repositorys.apps[key],"interface")
-        # if run backend, run it
-        if("backend" in repositorys.apps[key]):
-            run_app(repositorys.apps[key],"backend")
-    else :
-        print("Not possible run the app with id :", indexOfApp)
+add_apps = input("Do you want to add apps to running? (y/n): ")
+
+# List of apps available
+for id_unique, app in enumerate(repositorys.apps.keys()):
+    print(f"\n{id_unique} - {app}")
+
+# Select apps to run
+selected_apps = input(f"\nselect the apps with format : '0,1,2':\n");
+
+# Running default repositorys
+for i in repositorys.data:
+    print(f"prepare to run {i['type']} on {i['directory']}")
+    methods.wait(5)
+    methods.run_service(path_script, i['directory'], i['flag'])
+
+if add_apps == 'y':
+
+    # Worker selected apps
+    for i in selected_apps.split(','):  
+        try:
+            index_of_app = int(i)
+            list_of_apps = list(repositorys.apps.keys())
+            if(index_of_app < len(list_of_apps)):
+                key = list_of_apps[int(index_of_app)]
+                # if have docker, run it
+                if(key in repositorys.docker.keys()):
+                    run_app(repositorys.docker[key], 'docker')
+                #  if have interface, run it
+                if("interface" in repositorys.apps[key]):
+                    run_app(repositorys.apps[key],"interface")
+                # if run backend, run it
+                if("backend" in repositorys.apps[key]):
+                    run_app(repositorys.apps[key],"backend")
+        except:
+            print("An exception occurred::", index_of_app)
